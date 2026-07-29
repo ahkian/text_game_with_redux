42a403007dcab46c383157947e67c9b324fd2b1b
ahkian <dbrazel4@gmail.com>
2026-07-29 14:33:57 -0400

    Added reducer export that had been overlooked and tested out new Activity component

--- app/prologue/page.tsx
+++ app/prologue/page.tsx
@@ -1,22 +1,48 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useState, Activity } from "react";
 import { useRouter } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { takeDamage, healDamage } from "@/lib/features/player/playerSlice";
 
 export default function Prologue() {
     const router = useRouter();
     const dispatch = useAppDispatch();
     const player = useAppSelector((state) => state.player);
+    const playerClass = player.className;
 
-    // Debug to confirm Redux state is updated/persisted across navigation
-    useEffect(() => {
-        console.log('Prologue player state:', player);
-    }, [player]);
+   const takeAHit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        dispatch(takeDamage(1))
+   }
+
+   const healAPoint = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        dispatch(healDamage(1))
+   }
 
     return(
         <div className="content-center">
-            <h2>{player.name}</h2>
+            <Activity mode={playerClass === "Brawler" ? 'visible' : 'hidden'}>
+                You are a Brawler
+                <br/>
+                <button className="pl-2 pr-2 bg-transparent rounded border border-red-500 mb-3" onClick={takeAHit}>Take a hit</button>
+                <button className="pl-2 pr-2 bg-transparent rounded border border-blue-500" onClick={healAPoint}>Heal a point</button>
+            </Activity>
+            <Activity mode={playerClass === "Engineer" ? 'visible' : 'hidden'}>
+                You are an Engineer
+                <br/>
+                <button className="pl-2 pr-2 bg-transparent rounded border border-red-500 mb-3" onClick={takeAHit}>Take a hit</button>
+                <br/>
+                <button className="pl-2 pr-2 bg-transparent rounded border border-blue-500" onClick={healAPoint}>Heal a point</button>
+            </Activity>
+            <Activity mode={playerClass === "Thief" ? 'visible' : 'hidden'}>
+                You are a Thief
+                <br/>
+                <button className="pl-2 pr-2 bg-transparent rounded border border-red-500 mb-3" onClick={takeAHit}>Take a hit</button>
+                <br/>
+                <button className="pl-2 pr-2 bg-transparent rounded border border-blue-500" onClick={healAPoint}>Heal a point</button>
+            </Activity>
         </div>
     )
 }
