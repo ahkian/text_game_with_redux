3960317111566ce5f7015939015ebe5a0d8071d0
ahkian <dbrazel4@gmail.com>
2026-08-31 16:52:16 -0400

    Started the story

--- app/prologue/page.tsx
+++ app/prologue/page.tsx
@@ -23,15 +23,16 @@ export default function Prologue() {
 
     return(
         <div className="content-center">
+            <h1 className="flex justify-center underline font-bold">Prologue</h1> 
             <Activity mode={playerClass === "Brawler" ? 'visible' : 'hidden'}>
-                <h2 className="flex justify-center underline bold">Brawler Prologue</h2>                
+                <p>The screams are the first indication you have that something is wrong. Your planned peaceful evening of relaxing at home with whatever show doesn't look like complete shit on Verteron+ has gone up in smoke. You leave your apartment to see your neighbors clustered around the windows looking out at your apartment block's courtyard. Shouldering people out of the way you get close enough to the window to see what's going on. There is a crowd of people running in a panic and a few yards behind them giving chase is a gargantuan figure that you barely recognize as human thanks to their extensive cyberware.</p>               
             </Activity>
             <Activity mode={playerClass === "Engineer" ? 'visible' : 'hidden'}>
-                <h2 className="flex justify-center underline bold">Engineer Prologue</h2>
+                <p></p>
                 
             </Activity>
             <Activity mode={playerClass === "Thief" ? 'visible' : 'hidden'}>
-                <h2 className="flex justify-center underline bold">Thief Prologue</h2>
+                <p></p>
                 
             </Activity>
         </div>
