cdc755cf59a3a4563ce5edaf288c238a19274c61
ahkian <dbrazel4@gmail.com>
2026-09-23 15:17:29 -0400

    built out more of the combat page

--- app/combat/page.tsx
+++ app/combat/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation"
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { takeDamage, healDamage } from "@/lib/features/player/playerSlice";
+import { Activity } from "react";
 
 interface combatProps {
     enemyId: string;
@@ -14,4 +15,19 @@ export default function Combat({enemyId, nextPage}:combatProps){
     const dispatch = useAppDispatch();
     const player = useAppSelector((state) => state.player)
     const enemy = useAppSelector((state) => Object.values(state.npc.entities).find(npc => npc.id === enemyId))
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        router.push(nextPage)
+    }
+    if(!enemy){
+        return undefined
+    }
+    return(
+        <div>
+            <Activity mode={player.hp === 0 || enemy.hp === 0 ? 'visible' : 'hidden'}>
+                <button onClick={handleClick}></button>
+            </Activity>
+        </div>
+    )
 }
